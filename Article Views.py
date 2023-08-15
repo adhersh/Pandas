68893b0caf0638e17da18610c5cc65cf951e2d2e
adhersh <unniadhersh@gmail.com>
2023-08-15 15:10:09 +0530

    new file:   Article Views.py

---
+++ Article Views.py
@@ -0,0 +1,56 @@
+""" Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+Output: 
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+ """
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    merged_df = views.merge(views,how="inner",left_on=['author_id','viewer_id'],right_on=['viewer_id','author_id'])
+    result_df = merged_df[merged_df['author_id_x'] == merged_df['viewer_id_x']]
+    result_df=result_df[['author_id_x']].drop_duplicates()
+    result_df.columns=['id']
+    result_df= result_df.sort_values(by='id')
+    return result_df
+    
